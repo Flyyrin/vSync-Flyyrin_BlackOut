ebf925924817a4d5f4eaebdc9acaa8e61a7c1600
Flyyrin <66778328+Flyyrin@users.noreply.github.com>
2021-04-10 23:12:38 +0200

    vSync working with Flyyrin_BlackOut

---
+++ __resource.lua
@@ -0,0 +1,10 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_scripts {
+    'vs_client.lua',
+    '@Flyyrin_BlackOut/server.lua' --Added by github.com/Flyyrin for Flyyrin_BlackOut
+}
+
+server_scripts {
+    'vs_server.lua',
+}
